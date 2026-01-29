03ac6eaf70e4a8d8e710d3c1acbe90298fdb41c3
Fran Aguilera <fam0016@alu.medac.es>
2026-01-29 12:50:23 +0100

    add SQL script for database schema initialization

---
+++ src/main/resources/sql/script.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA `user_core_db` ;
+
+CREATE TABLE `user_core_db`.`users` (
+  `id_user` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(60) NOT NULL,
+  `last_name` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(250) NOT NULL,
+  `age` INT NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
