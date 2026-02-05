48f9e34bd6a1f1d60764462baa75d10caf127568
Fran Aguilera <fam0016@alu.medac.es>
2026-02-05 15:00:28 +0100

    define UserDao interface for CRUD operations

--- src/main/java/io/github/aguileramesa91/usercore/dao/UserDao.java
+++ src/main/java/io/github/aguileramesa91/usercore/dao/UserDao.java
@@ -1,4 +1,13 @@
 package io.github.aguileramesa91.usercore.dao;
 
-public class UserDao {
+import io.github.aguileramesa91.usercore.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void addUser (User user);
+    User findById (Long id);
+    List<User> findAll();
+    void updateUser (User user);
+    void deleteUser (Long id);
 }
