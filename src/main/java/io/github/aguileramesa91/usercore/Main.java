c9ba3bcc0ea0e5e3e4aa53f12dd43e104ecaa75e
Fran Aguilera <fam0016@alu.medac.es>
2026-02-05 16:10:45 +0100

    verify database persistence with functional addUser and Main test

--- src/main/java/io/github/aguileramesa91/usercore/Main.java
+++ src/main/java/io/github/aguileramesa91/usercore/Main.java
@@ -1,16 +1,26 @@
 package io.github.aguileramesa91.usercore;
 
 import io.github.aguileramesa91.usercore.config.DatabaseConfig;
+import io.github.aguileramesa91.usercore.dao.UserDao;
+import io.github.aguileramesa91.usercore.dao.UserDaoDatabase;
+import io.github.aguileramesa91.usercore.model.User;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args){
-        try (Connection connection = DatabaseConfig.getConnection()){
-            System.out.println("Connection established");
-        } catch (SQLException e){
-            System.err.println("Connection error: " + e.getMessage());
+        UserDao userDao = new UserDaoDatabase ();
+        User user1 = new User();
+        user1.setName("John");
+        user1.setLastName("Williams");
+        user1.setEmail("jwill@test.com");
+        user1.setAge(19);
+
+        try{
+            userDao.addUser(user1);
+        } catch (RuntimeException e) {
+            System.out.println("Error saving user: " + e.getMessage());
         }
     }
 }
