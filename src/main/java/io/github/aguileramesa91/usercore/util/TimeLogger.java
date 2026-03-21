2af911798483bf2a3e2b24bd07f89c74366d8b99
Francisco Aguilera <aguilera.mesa.f.j@gmail.com>
2026-03-21 09:02:19 +0100

    implement presistent file logging anda update gitignore for log files

--- src/main/java/io/github/aguileramesa91/usercore/util/TimeLogger.java
+++ src/main/java/io/github/aguileramesa91/usercore/util/TimeLogger.java
@@ -1,19 +1,35 @@
 package io.github.aguileramesa91.usercore.util;
 
+import javax.imageio.IIOException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeLogger {
 
+    private static final String FILE_NAME = "execution_logs.txt";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
 
     public static void logExecutionTime(String operation, long startTime){
         long duration = System.currentTimeMillis() - startTime;
         String timestamp = LocalDateTime.now().format(formatter);
 
         String logMessage = String.format("[%s] OPERATION: %s | DURATION: %d ms",
-                                          timestamp, operation, duration);
-
+                timestamp, operation, duration);
         System.out.println("\u001B[36m" + logMessage + "\u001B[0m");
+
+        try (FileWriter fw = new FileWriter((FILE_NAME), true);
+             PrintWriter out = new PrintWriter(fw)) {
+
+            out.println(logMessage);
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
     }
+
+
 }
